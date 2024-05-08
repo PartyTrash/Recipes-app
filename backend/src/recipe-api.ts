34c04d3045bacc3255c8e43bb85a1e5fcadd055b
PartyTrash <dima.smirnov.2020@inbox.ru>
2024-05-08 14:58:14 +0300

    first commit

---
+++ backend/src/recipe-api.ts
@@ -0,0 +1,63 @@
+const apiKey = process.env.API_KEY;
+
+export const searchRecipes = async (seacrhTerm: string, page:number) => {
+    if (!apiKey) {
+        throw new Error ('API Key not found')
+    }
+
+    const url = new URL("https://spoonacular.com/recipes/complexSearch");
+
+    const queryParams = {
+        apiKey,
+        query: seacrhTerm,
+        number: '10',
+        offset:(page * 10).toString()
+    }
+    url.search = new URLSearchParams(queryParams).toString()
+
+    try{
+        const searchResponse = await fetch(url);
+        const resultsJson = await searchResponse.json()
+    } catch(error) {
+        console.log(error)
+    }
+};
+
+
+
+export const getRecipeSummary = async (recipeId:string) => {
+    if (!apiKey){
+        throw new Error ('API Key not found');
+    }
+
+
+    const url = new URL(`https://api.spoonacular.com/recipes/${recipeId}/summary`)
+
+const params = {
+    apiKey:apiKey
+}
+url.search = new URLSearchParams(params).toString();
+
+const response = await fetch(url);
+const json = await response.json();
+
+return json;
+};
+
+export const getFavouriteRecipesByIDs = async(ids:string[]) => {
+    if (!apiKey) {
+        throw new Error ('API Key not found')
+    }
+
+    const url = new URL ("https://api.spoonacular.com/recipes/informationBulk");
+    const params = {
+        apiKey: apiKey,
+        ids: ids.join(',')
+    }
+    url.search = new URLSearchParams(params).toString()
+
+    const searchResponse = await fetch(url);
+    const json = await searchResponse.json();
+
+    return {results: json};
+}
